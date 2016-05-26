c74249a49d78929c44768086f405acf9e29b4da4
Steven Atkinson <steven.atkinson@gmx.com>
2016-05-26 12:53:54 +0100

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'sinatra', '1.4.6', require: 'sinatra/base'
+gem 'sinatra-contrib', '1.4.6'
+gem 'rake'
+gem 'json'
+gem 'puma'
+
+gem 'httparty', '0.13.7'
---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'bundler'
+Bundler.require :default
+
+# Main app class
+class App < Sinatra::Base
+  set :environment, :development
+  set :session, false
+  set :method_override, true
+  set :root, File.dirname(__FILE__)
+  set :run, false
+  set :bind, '0.0.0.0'
+  set :port, 3000
+  set :server, :puma
+
+  get '/' do
+    response = HTTParty.get('http://programmingexcuses.com/')
+    pattern = %r(\<a href="\/" rel="nofollow".*?\>(.*?)\<)
+    content = pattern.match(response.body)[1]
+    json message: content
+  end
+
+  run! if app_file == $PROGRAM_NAME
+end
